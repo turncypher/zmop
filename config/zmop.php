5570a8a2d7f0013de90da9f83a2abf1f77e0e7f0
turncypher <turncypher@gmail.com>
2016-10-15 17:03:10 +0800

    change config file

--- config/zmop.php
+++ config/zmop.php
@@ -3,8 +3,18 @@
  * Created by IntelliJ IDEA.
  * User: root
  * Date: 16-10-15
- * Time: 上午11:37
+ * Time: 下午2:12
  */
+
 return [
-    'app_id' => "213",
+
+    "gatewayUrl" => "https://zmopenapi.zmxy.com.cn/openapi.do",
+    //芝麻分配给商户的 appId
+    "appId" => "1000997",
+    //数据编码格式
+    "charset" => "UTF-8",
+    //商户私钥文件
+    "privateKeyFile" => storage_path("zhima/rsa_private_key.pem"),
+    //芝麻公钥文件
+    "zmPublicKeyFile" => storage_path("zhima/zhima_public_key.pem"),
 ];
