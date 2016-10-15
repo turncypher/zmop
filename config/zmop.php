fd7c455cf56cb662d79dbf793c86f116807e0fc5
turncypher <turncypher@gmail.com>
2016-10-15 17:42:08 +0800

    c

--- config/zmop.php
+++ config/zmop.php
@@ -10,11 +10,11 @@ return [
 
     "gatewayUrl" => "https://zmopenapi.zmxy.com.cn/openapi.do",
     //芝麻分配给商户的 appId
-    "appId" => "1000997",
+    "appId" => "your-app-id",
     //数据编码格式
     "charset" => "UTF-8",
     //商户私钥文件
-    "privateKeyFile" => storage_path("zhima/rsa_private_key.pem"),
+    "privateKeyFile" => "path/to/your/private_key.pem",
     //芝麻公钥文件
-    "zmPublicKeyFile" => storage_path("zhima/zhima_public_key.pem"),
+    "zmPublicKeyFile" => "path/to/your/public_key.pem",
 ];
